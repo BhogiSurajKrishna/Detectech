832671f73183feb65cf6e3f177e64be756d83ae3
BhogiSurajKrishna <55207865+BhogiSurajKrishna@users.noreply.github.com>
2020-06-14 21:47:22 -0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 #from rest_framework import routers
 #from .views import AuthViewSet
-from .views import loginAPIView,registerAPIView,logoutAPIView,changepassAPIView,forgotpassAPIView,generalAPIView
+from .views import loginAPIView,registerAPIView,logoutAPIView,changepassAPIView,forgotpassAPIView,generalAPIView,create_clientAPIView
 # router = routers.DefaultRouter()
 # router.register('', AuthViewSet, basename='auth')
 
@@ -18,6 +18,8 @@ urlpatterns = [
 	path('changepass/',changepassAPIView.as_view(),name='changepass'),
 	path('forgotpass/',forgotpassAPIView.as_view(),name='forgotpass'),
 	path('general/',generalAPIView.as_view(),name='general'),
+	path('cre_client/',create_clientAPIView.as_view(),name='cre_client'),
+	#path('cre_section/',create_sectionAPIView.as_view(),name='cre_section'),
 
 ]
 #RiuRxJHqCeWzf1bGG7fy2jMPamyVsu5GfjbzlIja - AWS secret access key
