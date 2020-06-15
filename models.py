782dc105a65226468ab1554c14feebad52fe7fa0
BhogiSurajKrishna <55207865+BhogiSurajKrishna@users.noreply.github.com>
2020-06-14 21:45:28 -0700

    Update models.py

--- models.py
+++ models.py
@@ -16,3 +16,8 @@ class CustomUser(AbstractUser):
 
 	def __str__(self):
 		return f"{self.email} - {self.username}"
+
+class Client(models.Model):
+	user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+	name = models.CharField(max_length=40)
+	location = models.CharField(max_length=100)
